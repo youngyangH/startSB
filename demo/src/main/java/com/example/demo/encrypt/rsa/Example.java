7f2a21cfa80dfd71f193f0e06aeaab3bab28f0d4
young Huang <huangyangyang200@sina.cn>
2019-12-29 15:36:23 +0800

    add encrypt and dencrypt

---
+++ demo/src/main/java/com/example/demo/encrypt/rsa/Example.java
@@ -0,0 +1,49 @@
+package com.example.demo.encrypt.rsa;
+
+import sun.misc.BASE64Decoder;
+
+/** */
+
+/**
+ * <p>
+ * RSA公钥/私钥/签名工具包
+ * </p>
+ * <p>
+ * 罗纳德·李维斯特（Ron [R]ivest）、阿迪·萨莫尔（Adi [S]hamir）和伦纳德·阿德曼（Leonard [A]dleman）
+ * </p>
+ * <p>
+ * 字符串格式的密钥在未在特殊说明情况下都为BASE64编码格式<br/>
+ * 由于非对称加密速度极其缓慢，一般文件不使用它来加密而是使用对称加密，<br/>
+ * 非对称加密算法可以用来对对称加密的密钥加密，这样保证密钥的安全也就保证了数据的安全
+ * </p>
+ *
+ * @author IceWee
+ * @date 2012-4-26
+ * @version 1.0
+ */
+public class Example {
+
+
+
+    public static void main(String[] args) throws Exception {
+        String privateKey="MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm";
+        String source = "";
+        byte[] data = source.getBytes();
+        byte[] result = RSAUtils.decryptByPrivateKey(data, privateKey);
+        System.out.println(result.toString());
+        System.out.println(new BASE64Decoder().decodeBuffer(result.toString()));
+
+
+//        Map<String, Key> keyPair = RSAUtils.genKeyPair();
+//        System.out.println("publicKey: " + keyPair.get(RSAUtils.PUBLIC_KEY).getFormat());
+//        System.out.println("privateKey: " + keyPair.get(RSAUtils.PRIVATE_KEY).getEncoded().toString());
+
+//        System.out.println("原文字：\r\n" + source);
+//        byte[] data = source.getBytes();
+//        byte[] encodedData = RSAUtils.encryptByPrivateKey(data, privateKey);
+//        System.out.println("加密后：\r\n" + new String(encodedData));
+//        String base64EncodedData = Base64Utils.encodeToString(encodedData);
+//        //System.out.println("加密后BASE64：\r\n" + base64EncodedData);
+//        System.out.println("加密后BASE64：\r\n" + new BASE64Encoder().encode(encodedData));
+    }
+}
